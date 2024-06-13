4a71e3a9a8de8dfc9dc4410ac84b8d1a48b04b8f
Theo <tkbrinckman@gmail.com>
2024-06-13 15:57:15 -0400

    speed

--- Culminating.java
+++ Culminating.java
@@ -2,16 +2,23 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Culminating extends Applet implements KeyListener, Runnable {
-    Image background, apple, snakeUp, snakeDown, snakeLeft, snakeRight, currentSnake, obstacle;
-    int snakeX, snakeY, appleX, appleY, counter = 0, speed = 10, appletHeight, appletWidth;
+public class Culminating extends Applet implements KeyListener, Runnable, MouseListener, MouseMotionListener {
+    Image background, apple, snakeUp, snakeDown, snakeLeft, snakeRight, currentSnake, obstacle, snakeBody;
+    int snakeX, snakeY, appleX, appleY, counter = 0, speed = 35, appletHeight, appletWidth, mouseX, mouseY;
     String direction = "right";
     Graphics bufferGraphics;
     Image offscreen;
+    boolean isDead = false;
+    int[] bodyX = new int[100];
+    int[] bodyY = new int[100];
+    int bodyLength = 1;
 
     public void init() {
         setBackground(Color.blue);
         addKeyListener(this);
+
+        addMouseListener(this);
+        addMouseMotionListener(this);
         background = this.getImage(this.getDocumentBase(), "Grass_Sample.png");
         apple = this.getImage(this.getDocumentBase(), "apple.png");
         snakeUp = this.getImage(this.getDocumentBase(), "snake-up.png");
@@ -19,11 +26,12 @@ public class Culminating extends Applet implements KeyListener, Runnable {
         snakeLeft = this.getImage(this.getDocumentBase(), "snake-left.png");
         snakeRight = this.getImage(this.getDocumentBase(), "snake-right.png");
         currentSnake = this.getImage(this.getDocumentBase(), "snake-right.png");
+        snakeBody = this.getImage(this.getDocumentBase(), "snake-body.png");
         obstacle = this.getImage(this.getDocumentBase(), "obstacle.png");
         snakeX = 50;
         snakeY = 50;
-        appleX = (int) (Math.random() * 1400);
-        appleY = (int) (Math.random() * 640);
+        appleX = (int) (Math.random() * 1350);
+        appleY = (int) (Math.random() * 590);
         appletHeight = 640;
         appletWidth = 1408;
 
@@ -39,9 +47,12 @@ public class Culminating extends Applet implements KeyListener, Runnable {
         bufferGraphics.drawImage(background, 704, 0, this);
         bufferGraphics.drawImage(obstacle, 600, 160, 150, 300, this);
         bufferGraphics.drawImage(apple, appleX, appleY, 50, 50, this);
-        bufferGraphics.drawImage(currentSnake, snakeX, snakeY, 50, 50, this);
+        bufferGraphics.drawImage(currentSnake, snakeX, snakeY, 35, 35, this);
         bufferGraphics.drawString("Score " + counter, 20, 20);
-        if (speed == 0) {
+        for (int i = 0; i < bodyLength; i++) {
+            bufferGraphics.drawImage(snakeBody, bodyX[i], bodyY[i], 35, 35, this);
+        }
+        if (isDead == true) {
             bufferGraphics.drawString("Game over", 600, 280);
             bufferGraphics.setColor(Color.red);
             bufferGraphics.fillRect(550, 300, 150, 75); // draws a square at bX and bY
@@ -64,6 +75,44 @@ public class Culminating extends Applet implements KeyListener, Runnable {
         myAnimation.start();
     }
 
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    // MouseReleased method
+    public void mouseReleased(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+
+        // Check if the mouse click is inside the green square
+
+        if ((mouseX >= 550) && (mouseX <= 550 + 150) &&
+                (mouseY >= 300) && (mouseY <= 300 + 75)) {
+            bodyLength = 1;
+            snakeX = 0;
+            snakeY = 0;
+            isDead = false;
+            counter = 0;
+        }
+
+        repaint(); // Repaint the applet to update the score
+        e.consume();
+    }
+
     public void keyTyped(KeyEvent e) {
         // do nothing
     }
@@ -74,21 +123,23 @@ public class Culminating extends Applet implements KeyListener, Runnable {
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
         System.out.println("you pressed " + key);
-        if (key == 38) {
-            currentSnake = snakeUp;
-            direction = "up";
-        }
-        if (key == 40) {
-            currentSnake = snakeDown;
-            direction = "down";
-        }
-        if (key == 37) {
-            currentSnake = snakeLeft;
-            direction = "left";
-        }
-        if (key == 39) {
-            currentSnake = snakeRight;
-            direction = "right";
+        if (isDead == false) {
+            if (key == 38 && direction != "bottom") {
+                currentSnake = snakeUp;
+                direction = "up";
+            }
+            if (key == 40) {
+                currentSnake = snakeDown;
+                direction = "down";
+            }
+            if (key == 37) {
+                currentSnake = snakeLeft;
+                direction = "left";
+            }
+            if (key == 39) {
+                currentSnake = snakeRight;
+                direction = "right";
+            }
         }
         repaint();
         e.consume();
@@ -98,50 +149,72 @@ public class Culminating extends Applet implements KeyListener, Runnable {
 
         while (true) {
 
-            if (collision(snakeX, snakeY, 50, 50, appleX, appleY, 50, 50)) {
+            if (collision(snakeX, snakeY, 35, 35, appleX, appleY, 50, 50)) {
                 counter = counter + 1;
-                speed = speed + 5;
-                appleX = (int) (Math.random() * 1400);
-                appleY = (int) (Math.random() * 640);
+                bodyX[bodyLength] = 0;
+                bodyY[bodyLength] = 0;
+                bodyLength = bodyLength + 1;
+                appleX = (int) (Math.random() * 1350);
+                appleY = (int) (Math.random() * 590);
                 repaint();
 
             }
             if (collision(appleX, appleY, 50, 50, 600, 160, 150, 300)) {
-                appleX = (int) (Math.random() * 1400);
-                appleY = (int) (Math.random() * 640);
+                appleX = (int) (Math.random() * 1350);
+                appleY = (int) (Math.random() * 590);
                 repaint();
 
             }
-            if (collision(snakeX, snakeY, 50, 50, 600, 160, 150, 300)) {
-                snakeX = 10;
-                snakeY = 10;
-                speed = 0;
+
+            for (int i = 2; i < bodyLength - 1; i++) {
+                if (collision(snakeX, snakeY, 35, 35, bodyX[i], bodyY[i], 35, 35) && isDead == false) {
+                    isDead = true;
+                    counter = 0;
+                    repaint();
+
+                }
             }
 
-            if (snakeX > 704 * 2 - 50) {
+            for (int i = bodyLength - 1; i > 0; i--) {
+                bodyX[i] = bodyX[i - 1];
+                bodyY[i] = bodyY[i - 1];
+            }
+            bodyX[0] = snakeX;
+            bodyY[0] = snakeY;
+
+            if (collision(snakeX, snakeY, 35, 35, 600, 160, 150, 300)) {
+                snakeX = 10;
+                snakeY = 10;
+                isDead = true;
+            }
+
+            if (snakeX > 704 * 2 - 35) {
                 snakeX = 0;
             }
             if (snakeX < 0) {
-                snakeX = 704 * 2 - 50;
+                snakeX = 704 * 2 - 35;
             }
             if (snakeY < 0) {
-                snakeY = 320 * 2 - 50;
+                snakeY = 320 * 2 - 35;
             }
-            if (snakeY > 320 * 2 - 50) {
+            if (snakeY > 320 * 2 - 35) {
                 snakeY = 0;
             }
-            if (direction == "up") {
-                snakeY = snakeY - speed;
-            } else if (direction == "right") {
-                snakeX = snakeX + speed;
-            } else if (direction == "down") {
-                snakeY = snakeY + speed;
-            } else if (direction == "left") {
-                snakeX = snakeX - speed;
+            if (isDead == false) {
+                if (direction == "up") {
+                    snakeY = snakeY - speed;
+                } else if (direction == "right") {
+                    snakeX = snakeX + speed;
+                } else if (direction == "down") {
+                    snakeY = snakeY + speed;
+                } else if (direction == "left") {
+                    snakeX = snakeX - speed;
+                }
             }
+
             repaint(); // calls the paint method to be run.
             try {
-                Thread.sleep(100);
+                Thread.sleep(150);
             } catch (InterruptedException e) {
             }
         }
